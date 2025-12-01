23531427ab487a9b005c489129663c0f1513ed99
AislanJefferson <aislanjsb@gmail.com>
2025-12-01 16:34:00 -0300

    Adicionado comentario javadoc

--- src/main/Projeto.java
+++ src/main/Projeto.java
@@ -2,19 +2,27 @@ package main;
 
 import java.util.Scanner;
 
+/**
+ * Classe de Inicio do projeto.
+ * <p>
+ * Esta classe é utilizada para a interface via terminal do projeto
+ * </p>
+ *
+ * @author Wislley Rennardy
+ */
 public class Projeto {
 	private Scanner sc;
-	
-	public Projeto () {
+
+	public Projeto() {
 		this.sc = new Scanner(System.in);
 	}
-	
+
 	public void inicia() {
 		System.out.println("Seu nome?");
 		String texto = sc.nextLine();
 		System.out.printf("Bem vindo %s!", texto);
 	}
-	
+
 	public static void main(String[] args) {
 		Projeto projeto = new Projeto();
 		projeto.inicia();
